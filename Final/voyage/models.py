2e74509761fe77fc034675a86221bf5958f1c5ef
JMeni01 <129964309+JMeni01@users.noreply.github.com>
2023-04-17 16:17:20 +0200

    Update models.py

--- Final/voyage/models.py
+++ Final/voyage/models.py
@@ -2,8 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Trip(models.Model):
+    
     destination = models.CharField(max_length=30)
+    
     time = models.CharField(max_length=30)
+    
+    def __str__(self):
+        
+        return self.destination
 
-def __str__(self):
-    return self.destination
+class Transportation(models.Model):
+    
+    
