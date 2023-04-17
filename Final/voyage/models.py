de17ee09364fd349ff9a50b4709da4eaae80bd97
jessicarosseau <jrosseau@uvm.edu>
2023-04-17 15:33:03 +0200

    Added a trip class, that has a destination and a time

--- Final/voyage/models.py
+++ Final/voyage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Trip(models.Model):
+    destination = models.CharField(max_length=30)
+    time = models.CharField(max_length=30)
+
+def __str__(self):
+    return self.destination
