a215cd8b5dc2263cc689d4e39be7b51766f1c62d
JMeni01 <129964309+JMeni01@users.noreply.github.com>
2023-04-19 12:53:03 +0200

    Update models.py

--- Final/voyage/models.py
+++ Final/voyage/models.py
@@ -13,4 +13,47 @@ class Trip(models.Model):
 
 class Transportation(models.Model):
     
+    number_buses = models.IntegerField(max_length=10)
     
+    cost_transportation = models.IntegerField(max_length=100)
+    
+    def __str__(self):
+        
+        return self.destination
+    
+class Lunch(models.Model):
+    
+    name_restourant_lunch = models.CharField(max_length=30)
+    
+    cost_lunch = models.IntegerField(max_length=1000)
+    
+    satisfaction_lunch = models.IntegerField(max_length=5)
+    
+    def __str__(self):
+        
+        return self.destination
+
+
+class Dinner(models.Model):
+    
+    name_restourant_dinner = models.CharField(max_length=30)
+    
+    cost_dinner = models.IntegerField(max_length=100)
+    
+    satisfaction_dinner = models.IntegerField(max_length=5)
+    
+    def __str__(self):
+        
+        return self.destination
+    
+class Activities(models.Model):
+    
+    name_activity = models.CharField(max_length=30)
+    
+    cost_activity = models.IntegerField(max_length=100)
+    
+    satisfaction_activity = models.IntegerField(max_length=5)
+    
+    def __str__(self):
+        
+        return self.destination
