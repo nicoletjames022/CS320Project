35632f8c4776a62a50a5908a944c5c02ddb029aa
jessicarosseau <jrosseau@uvm.edu>
2023-04-24 16:26:15 +0200

    Added to users folder

--- Final/users/urls.py
+++ Final/users/urls.py
@@ -22,6 +22,7 @@ app_name = "users"
 urlpatterns = [
 
     path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register')
     
 ]
 
--- Final/users/views.py
+++ Final/users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import login
+
+from django.contrib.auth.forms import UserCreationForm
+
+from .forms import PersonForm
 
 # Create your views here.
+def register(request):
+
+    if request.method != 'POST':
+        form = UserCreationForm()
+        personForm = PersonForm()
+
+    else:
+        form = UserCreationForm(date=request.POST)
+        personForm = PersonForm(data=request.POST)
+
+        if form.is_valid() and personForm.is_valid():
+
+            savedUser = form.save()
+
+            person = personForm.save(commit=False)
+
+            person.user = savedUser
+
+            personForm.save()
+
+            return redirect('users:login')
+    context = {'form': form, 'person_form' : personForm}
+
+    return render(request, "registration/registration.html", context)
